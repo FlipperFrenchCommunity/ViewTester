f227c099a8ad704484d095267a0c3275c3f138fb
Rom1 <rom1@canel.ch>
2022-11-04 19:24:17 +0100

    documente la partie pour afficher une vue

--- README.md
+++ README.md
@@ -1,13 +1,26 @@
 # Flipper Zero - Test des vues
 ## But
-Approfondir le service **GUI** en découvrant les vues (*views*) et la méthode
-d'attachements avec **view_dispatcher**.
+Approfondir le service **GUI** en découvrant les vues (*views*) et la méthode d'attachements avec **view_dispatcher**.
 
 ## Présentation
+**View_dispatcher** est une sur couche à la triplette **GUI**, **cavnas** et **view_port**. L'environment d'avant était un peu austère, il n'y avait que deux fonctions de rappel. Une pour gérer l'affichage et l'autre pour traiter les entrées.
+
+Maintenant avec **View_dispatcher**, il a un système de vues (*views*) pour simplifier la composition à afficher. Il en gère et définit quand les afficher (il les dispatche). Il simplifie aussi toute la gestion des événements entrant et facilite la navigation entre les différentes vues.
 ![view dispatcher](./assets/view_dispatcher.png)
 
-### Modules
-Liste de modules de vues:
-|submenu| Pour faire des sous-menu|
-|dialogue| Faire une fenêtre intempestive pour dialoguer avec l'utilisateur|
+### Première étape pour afficher et quitter.
+Comme d'habitude, il faut commencer par allouer les ressources. La première partie concernant l'interface graphique GUI a été abordée dans la [partie 1](https://github.com/FlipperFrenchCommunity/BoxMover), il faut donc se référer à celle-ci pour plus d'information.
 
+Ici, view_port est donc remplacé par view_dispatcher. Un fois sa structure d'état allouée il faudra aussi l'attacher à l'interface graphique. Il n'y a plus besoin d'allouer une file de messages (queue) provenant de **Furi**. Une file peut être directement définie en interne avec la fonction ***view_dispatcher_enable_queue()***. Cette fonction se charge de la créer avec **Furi**. Cette file est aussi utilisée pour transmettre les événements.
+
+La première vue peut être créée en commençant par lui allouer les ressource. Comme pour view_port, il faut lui passer une fonction de rappel pour modifier l'affichage avec la fonction ***view_set_draw_callback()*** et une autre pour gérer les événements en entrées avec ***view_set_input_callback()***. Ces deux fonction font appel à leur fonction respective: ***view_port_draw_callback_set()*** et ***view_port_input_callback_set()***.
+
+Dans la fonction de rappel coder l'affichage, une simple fonction ***canvas_draw_str()*** provenant de **canvas** affichera une chaîne de caractères.
+
+La détection des entrées de l'utilisateur est simplifiée. En utilisant la fonction ***view_set_previous_callback()***, une fonction de rappel sera définit pour afficher la prochaine vue lorsque la touche retour sera pressée. Une valeur de retour contant la prochaine vu est retournée. Ici c'est la constante **VIEW_NONE** aui est retournée et elle signale au dispatcher de se terminer proprement. Avec cette solution nous n'avons pas besoin d'utilise de fonction de rappel pour les événements en entrée vu que nous pouvons quitter le programme.
+
+Maintenant, que la vue est crée, il est possible de l'ajouter au dispatcher avec ***view_dispatcher_add_view()*** et de lui dire de l'afficher avec ***view_dispatcher_switch_to_view()***.
+
+Il ne reste plus qu'a démarrer le dispatcher dans la fonction principale avec ***view_dispatcher_run()***
+
+Bien sur, il ne faut pas oublier de libérer les ressources à la fin du programme pour éviter les fuites de mémoires.
--- view_tester.c
+++ view_tester.c
@@ -1,87 +1,115 @@
 /*
- * Project: Test View
- * File: view_test.c
+ * Project: View Tester
+ * File: view_tester.c
  * Create by: Rom1 <rom1@canel.ch> - Flipper French Community - https://github.com/FlipperFrenchCommunity
  * Date: 26 octobre 2022
  * License: GNU GENERAL PUBLIC LICENSE v3
- * Description: Décortiquer et apprendre à faire créer des vues.
+ * Description: Décortiquer et apprendre à créer des vues et de les dispatcher.
  */
 #include <furi.h>
 #include <gui/gui.h>
-#include <gui/view_dispatcher.h>
 #include <input/input.h>
 #include <stdlib.h>
 
+/* Ajouter l'en-tête pour view_dispatcher */
+#include <gui/view_dispatcher.h>
+
 typedef struct {
-    FuriMessageQueue* event_queue;
     FuriMutex* model_mutex;
 
     Gui* gui;
-    ViewDispatcher* view_dispatcher;
-    View* view;
+    ViewDispatcher* view_dispatcher;    // Ajoute la structure d'état pour le dispatcher
+    View* view;                         // Et la structure d'état pour la vue (courant?)
 } ViewTesterState;
 
-
+/**
+ * Fonction de rappel pour dessiner l'affichage
+ *
+ * @param       Canvas pour l'affichage de l'écran
+ * @param       _model ??
+ */
 static void view_dispatcher_view_draw_callback(Canvas* canvas, void* _model) {
     UNUSED(_model);
     canvas_draw_str(canvas, 5, 32, "H4ck the World! with Flipper");
 }
 
-static bool view_dispatcher_view_input_callback(InputEvent* event, void* context) {
-    UNUSED(event);
-    UNUSED(context);
-    return false;
-}
-
-
+/**
+ * Fonction de rappel pour terminer le service dispatcher. Elle retourne VIEW_NONE
+ * qui demande à view_dispatcher de se terminer proprement.
+ * 
+ * @param       Context contenant la variable d'état
+ */
 static uint32_t uart_echo_exit(void* context) {
     UNUSED(context);
     return VIEW_NONE;
 }
 
+/**
+ * Fonction pour initialiser le programme. Elle va allouer les différentes ressources 
+ * du programme dans la structure (ViewTesterState) contenant l'état du programme.
+ *
+ * @return      Structure contenant l'état du programme
+ */
 ViewTesterState* view_tester_alloc(){
     ViewTesterState* state = malloc(sizeof(ViewTesterState));
-
-    state->model_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
     state->gui = furi_record_open(RECORD_GUI);
 
-    state->event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
-    // View dispatcher
+    /*
+     * Allouer les ressources pour faire fonctionner view_dispatcher et les initialiser.
+     */
+    /* Alloue la structure d'état */
     state->view_dispatcher = view_dispatcher_alloc();
-    view_dispatcher_enable_queue(state->view_dispatcher);
+    /* Attache le dispatcher à la session graphique (GUI) */
     view_dispatcher_attach_to_gui(state->view_dispatcher, state->gui, ViewDispatcherTypeFullscreen);
+    /* Active une file de messages pour transmettre les événements */
+    view_dispatcher_enable_queue(state->view_dispatcher);
 
-    // View
+    /* Créer notre première vue 0 (view) en allouant la structure d'état */
     state->view = view_alloc();
+    /* Définir la fonction de rappel pour l'affichage. */
     view_set_draw_callback(state->view, view_dispatcher_view_draw_callback);
-    view_set_input_callback(state->view, view_dispatcher_view_input_callback);
+    /* Définir la fonction de rappel pour la bouton de retour. */
     view_set_previous_callback(state->view, uart_echo_exit);
 
-    // View Dispatcher attache la view
+    /* Ajouter la vue dans le dispatcher avec comme index 0 */
     view_dispatcher_add_view(state->view_dispatcher, 0, state->view);
+    /* Transmettre à view_port pour afficher la vue 0 */
     view_dispatcher_switch_to_view(state->view_dispatcher, 0);
     
+
     return state;
 }
 
+/**
+ * Fonction pour terminer le programme en libérant les ressources.
+ *
+ * @param       State contenant l'état du programme
+ */
 void view_tester_free(ViewTesterState* state){
+    /* Ne pas oublier de libérer les ressource crée précédemment */
     view_dispatcher_remove_view(state->view_dispatcher, 0);
     view_free(state->view);
     view_dispatcher_free(state->view_dispatcher);
+
+
     furi_record_close(RECORD_GUI);
-
-    furi_message_queue_free(state->event_queue);
-    furi_mutex_free(state->model_mutex);
-
     free(state);
 }
 
+
+/**
+ * Point d'entrée principal du programme.
+ */
 int32_t view_tester_app(void* p){
     UNUSED(p);
 
     ViewTesterState* state = view_tester_alloc();
-    view_dispatcher_run(state->view_dispatcher); // Démarre View Dispatcher
+
+    /* Démarrer view_dispatcher */
+    view_dispatcher_run(state->view_dispatcher);
+
+
     view_tester_free(state);
 
     return 0;
